11022e77a46a6ebf95b818ee581417585c396c3f
tamutamu <tamu.0.0.tamu@gmail.com>
2017-01-13 11:45:11 +0900

    Update.

--- prepare.sh
+++ prepare.sh
@@ -1,3 +1,5 @@
 #/bin/bash
 
-
+## For OpenJDK 1.6.0
+wget -q https://github.com/tamutamu-gradle-org/prep_travis/raw/master/UTN-DATACorpSGC.crt -O /tmp/UTN-DATACorpSGC.crt
+keytool -importcert -alias utndatacorpsgcca -file /tmp/UTN-DATACorpSGC.crt -keystore /etc/ssl/certs/java/cacerts -storepass changeit -trustcacerts -noprompt
