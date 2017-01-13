5d428d648da3cd41a103c6011721dcb5227e7ae1
tamutamu <tamu.0.0.tamu@gmail.com>
2017-01-13 13:29:52 +0900

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -3,3 +3,17 @@
 ## For OpenJDK 1.6.0
 wget -q https://github.com/tamutamu-gradle-org/prep_travis/raw/master/UTN-DATACorpSGC.crt -O /tmp/UTN-DATACorpSGC.crt
 sudo keytool -importcert -alias utndatacorpsgcca -file /tmp/UTN-DATACorpSGC.crt -keystore /etc/ssl/certs/java/cacerts -storepass changeit -trustcacerts -noprompt
+
+
+## NAF Install
+function clone_install(){
+  repo_dir=`echo $1 | sed -e 's|^.*/\(.*\)\.git|\1|'`
+  pushd repo_dir
+  git checkout develop
+  chmod +x gradlew
+  ./gradlew clean install
+  popd
+}
+
+clone_install https://github.com/nablarch/nablarch-core.git
+clone_install https://github.com/nablarch/nablarch-core-applog.git
